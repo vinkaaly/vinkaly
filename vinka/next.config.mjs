81fe0c9f825331a6b966fc95f2f287304a147b83
vinkaaly <valeyka.d@gmail.com>
2025-02-23 21:52:20 +0700

    Fix halaman utama Next.js

--- vinka/next.config.mjs
+++ vinka/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      appDir: true, // Pastikan ini ada kalau pakai App Router
+    }
+  }
+  
+  export default nextConfig
+  
